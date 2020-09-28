b1a95042c085b3e88e29826bcc57c9b150c06ff8
Michael Heinrichs <netopyr@users.noreply.github.com>
2020-09-28 10:31:04 +0200

    Implemented MVC

--- src/main/java/ch/fhnw/webec/converter/ConverterController.java
+++ src/main/java/ch/fhnw/webec/converter/ConverterController.java
@@ -1,16 +1,21 @@
 package ch.fhnw.webec.converter;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@Controller
 public class ConverterController {
 
-    public ModelAndView convertInches(double inches) {
+    @GetMapping("convertInches")
+    public ModelAndView convertInches(@RequestParam double inches) {
         final double result = inches * 2.54;
         final int cm = (int)result;
-        final int mm = (int)Math.round(10 * (result - cm));
+        final int mm = (int)Math.round(100 * (result - cm));
         final Map<String, Object> model = new HashMap<>();
         model.put("cm", cm);
         model.put("mm", mm);
